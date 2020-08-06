add9c028d43a70e9aaf44eed7a4a7f86be7c6a91
alex <reus.alex@gmail.com>
2020-08-06 11:38:10 +0200

    First refactor

--- src/src/domain/LightGrid.cs
+++ src/src/domain/LightGrid.cs
@@ -4,11 +4,31 @@ namespace ChristmasLightsKata.src.domain
 {
     public class LightGrid
     {
+        private bool[,] lightGrid = new bool[1000,1000];
 
+        public LightGrid()
+        {
+            for (int i = 0; i <= 999; i++)
+                for (int j = 0; j <= 999; j++)
+                    lightGrid[i, j] = true;
+
+            for (int k = 0; k <= 999; k++)
+                lightGrid[0, k] = !lightGrid[0, k];
+
+            for (int i = 499; i <= 500; i++)
+                for (int j = 499; j <= 500; j++)
+                    lightGrid[i, j] = false;
+        }
 
         public int CountLightLit()
         {
-            return 1000000 - 1000 - 4;
+            var count = 0;
+            for (int i = 0; i <= 999; i++)
+                for (int j = 0; j <= 999; j++)
+                    if (lightGrid[i, j])
+                        count++;
+
+            return count;
         }
 
     }
